b54a277f91347c2fe00a1d295834d91f55f2a592
JELYN <jelynobguia31@gmail.com>
2021-09-20 12:35:48 +0800

    first commit

---
+++ resources/views/pages/Home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/bootstrap-grid.min.css')}}">
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <title>Home</title>
+</head>
+<body>
+    <h1>PRELIM </h1>
+    Home | <a href="{{url('/Contact')}}">Contact</a>
+    | <a href="{{url('/Downloads')}}">Downloads</a>
+    | <a href="{{url('/About')}}">About Us</a>
+</body>
+</html>
